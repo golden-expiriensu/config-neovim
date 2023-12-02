dffaea7c66b4063c00d5d5c3edeebb3be8713c48
Danil Menkin <danil.menkin@arkis.xyz>
2023-12-02 22:52:22 +0400

    configured lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,4 +2,20 @@ require('lualine').setup({
     options = {
         theme = 'gruvbox-baby'
     },
+    sections = {
+        lualine_b = {
+            'branch',
+            'diff',
+            {
+                'diagnostics',
+                symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+            }
+        },
+        lualine_c = {
+            {
+                'filename',
+                path = 1
+            }
+        }
+    }
 })
