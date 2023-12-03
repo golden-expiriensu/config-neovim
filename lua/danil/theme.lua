d1c96b174a242a7f95c32b20217d6e1ef8ceacac
Danil Menkin <danil.menkin@arkis.xyz>
2023-12-03 19:33:39 +0400

    removed redundant theme config

--- lua/danil/theme.lua
+++ lua/danil/theme.lua
@@ -2,17 +2,3 @@ vim.g.gruvbox_baby_background_color = 'dark'
 vim.g.gruvbox_baby_telescope_theme = 1
 
 vim.cmd.colorscheme 'gruvbox-baby'
-
--- require('nordic').setup({
---     bold_keywords = true,
---     italic_comments = true,
---     transparent_background = true,
---     bright_border = true,
---     reduce_blue = false,
---     cursor_line = {
---         bold = true,
---         bold_number = true,
---     },
--- })
---
--- vim.cmd.colorscheme 'nordic'
