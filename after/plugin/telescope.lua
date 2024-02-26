6b4888fc88dc87f894a630ffdc70cd0cd14cd731
Danil Menkin <danil.menkin@arkis.xyz>
2024-02-26 19:20:46 +0700

    Add keymap for telescope to delete buffer

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,17 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+
+require('telescope').setup {
+    pickers = {
+        buffers = {
+            show_all_buffers = true,
+            sort_lastused = true,
+            mappings = {
+                i = {
+                    ["<c-d>"] = "delete_buffer",
+                }
+            }
+        }
+    }
+}
