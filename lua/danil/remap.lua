f1e96b7279482309f1e539a1422f6c5f1ef36ad5
Danil Menkin <gundarixdan@gmail.com>
2024-06-23 20:46:21 +0700

    Update quick fix controls

--- lua/danil/remap.lua
+++ lua/danil/remap.lua
@@ -21,10 +21,8 @@ vim.keymap.set('v', '<leader>d', '"_d')
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[/\<<C-r><C-w>\>/gI]])
 vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
