3e81be5b559f452dc307a5fbb7e065cece0bc750
Danil Menkin <gundarixdan@gmail.com>
2024-08-30 16:11:39 +0000

    Update default scratchpad

--- after/plugin/kulala.lua
+++ after/plugin/kulala.lua
@@ -46,15 +46,15 @@ require('kulala').setup({
     additional_curl_options = {},
     -- scratchpad default contents
     scratchpad_default_contents = {
-        "@MY_TOKEN_NAME=my_token_value",
-        "",
-        "# @name scratchpad",
-        "POST https://httpbin.org/post HTTP/1.1",
+        "POST https://blinknode-ws.blinkfinance.net HTTP/1.1",
         "accept: application/json",
         "content-type: application/json",
         "",
         "{",
-        '  "foo": "bar"',
+        '  "jsonrpc": "2.0",',
+        '  "method": "evm_setAutomine",',
+        '  "params": [true],',
+        '  "id": 0',
         "}",
     },
     -- enable winbar
